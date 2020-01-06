e0a8d33aeefc0ea34b85bfdc8b1f415e451ad689
Benjamin Small <ben20262@aim.com>
2020-01-06 21:27:30 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,19 @@
+class Genre
+  attr_reader :name
+  @@all = []
+
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Songs.all.select {|song| song.genre == self}
+  end
+
+  def artists
+    artist_array = songs.collect {|song| song.artist}
+    artist_array.uniq
+  end
+  
+end
